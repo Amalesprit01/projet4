1d69bb1953d087f2bd6a01ed3aec28030432976f
amal-selmi <amalselmi094@gmail.com>
2023-10-20 11:48:31 +0100

    second commit

---
+++ src/tn/esprit/gestionzoo/entities/Dolphin.java
@@ -0,0 +1,26 @@
+package tn.esprit.gestionzoo.entities;
+
+public class Dolphin extends Aquatic {
+    private float swimmingSpeed ;
+    public Dolphin(){
+        super();
+    }
+    public Dolphin (String family,String name,int age,boolean isMammal,String habitat,float swimmingSpeed ){
+        super(family, name, age, isMammal, habitat);
+        this.swimmingSpeed = swimmingSpeed ;
+    }
+    public float getSwimmingSpeed() {
+        return swimmingSpeed;
+    }
+    public void setSwimingSpeed (float swimmingSpeed){ this.swimmingSpeed = swimmingSpeed ;}
+
+    public void swim() {
+        System.out.println(" This dolphin is swimming ");
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + "swimingSpeed : " + swimmingSpeed ;
+    }
+
+}
--- src/tn/esprit/gestionzoo/main/Main.java
+++ src/tn/esprit/gestionzoo/main/Main.java
@@ -1,17 +1,52 @@
+package tn.esprit.gestionzoo.main;
+
+import tn.esprit.gestionzoo.entities.Animal;
+import tn.esprit.gestionzoo.entities.Zoo;
+import tn.esprit.gestionzoo.entities.Terrestrial;
+import tn.esprit.gestionzoo.entities.Aquatic;
+import tn.esprit.gestionzoo.entities.Dolphin;
+import tn.esprit.gestionzoo.entities.Penguin;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Entrée with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Animal lion = new Animal();
+        lion.setAge(5);
+        lion.setFamily("cats");
+        System.out.println(lion.getName());
+        Animal dog = new Animal("canine","snoopy",5,true);
+        System.out.println(dog.getName());
+        Zoo myZoo= new Zoo("wildlife","Ariana",25);
+        Zoo urZoo= new Zoo("jungle","america",20);
 
-        // Press Maj+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println(myZoo);
+       /* tn.esprit.gestionzoo.entities.Zoo.displayZoo();
 
-            // Press Maj+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+
+        System.out.println(myZoo);
+        System.out.println(myZoo.toString());
+        */
+        System.out.println(dog);
+        System.out.println(myZoo.addAnimal(lion));
+        System.out.println(myZoo.addAnimal(dog));
+        System.out.println(myZoo.searchAnimal(dog));
+
+        Animal chat = new Animal("chat", "chat", 6, true);
+        System.out.println(myZoo.searchAnimal(dog));
+        boolean removed = myZoo.removeAnimal(lion);
+        System.out.println(removed);
+        System.out.println(Zoo.comparerZoo(myZoo,urZoo));
+        System.out.println(myZoo.isZooFull(myZoo));
+        Aquatic aquatic = new Aquatic("Fish", "Sardine", 2, true, "Sea");
+        Terrestrial terrestrial = new Terrestrial("Panda", "Narla", 4, true, 2);
+        Dolphin dolphin = new Dolphin("Delphinidae", "Flipper", 5, true, "Ocean", 14.5f);
+        Penguin penguin = new Penguin("Spheniscidae", "Skipper", 3, true, "Ocean", 25.3f);
+        Aquatic aquatic1 = new Aquatic();
+        Dolphin dolphin1 = new Dolphin();
+        Penguin penguin1 = new Penguin();
+        aquatic1.swim();
+        dolphin1.swim();
+        penguin1.swim();
+        
     }
 }
